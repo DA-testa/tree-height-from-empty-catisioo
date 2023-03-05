5cf99005fd20feceb08110056cc4a066956b5194
Daniela Alise Smelte <74970648+catisioo@users.noreply.github.com>
2023-03-05 18:33:36 +0200

    Update tree_height.py

--- tree_height.py
+++ tree_height.py
@@ -6,55 +6,54 @@ import numpy as np
 
 
 def compute_height(n, parents):
-    arr = np.zeros(n, dtype=int)
-
-    for i in range(n):
-        if arr[i] != 0:
-            continue
-
-        height = 1
-        val = parents[i]
-        while val != -1:
-            if arr[val] != 0:
-                height = height + arr[val]
-                break
-            height = height + 1
-            val = parents[val]
-        a = i
-        while a != -1 and arr[a] == 0:
-            arr[a] = height
-            height = height - 1
-            a = parents[a]
-    return np.max(arr)
+    if n <= 100:
+        unique_array = np.unique(parents)
+        unique_number = unique_array.size
+        print(unique_number)
+    else:
+        pass
 
 
 def main():
     
     text = input()
 
+    #text
     if text == "I":
+        #nodes
         n = int(input())
-        parents = np.asarray(list(map(int, input().split())))
-    elif text == "F":
-        file = input().strip()
-        if "a" in file:
-            return
-        with open("test/" + file, mode= 'r') as f:
-            n = int(f.readline())
-            parents = np.asarray(list(map(int,f.readline().split())))
-    else:
-        return
+        #parents
+        string = input()
+        parents = np.asarray(list(map(int, string.split())))
+
+        if n == parents.size:
+            compute_height(n, parents)       
     
-    answer = compute_height(n, parents)
-    print(answer)
+    #file
+    elif text == "F":
+        file_n = input().strip()
+        if "a" in file_n:
+            return
+        else:
+            with open(".test/" + file_n, mode= 'r') as f:
+                #nodes
+                n = int(f.readline())
+                #parents
+                string = f.readline()
+                parents = np.asarray(list(map(int, string.split())))
 
-    #print(n)
-    #print(parents)
+                if n == parents.size:
+                    compute_height(n, parents)
+    
+    #answer = compute_height(n, parents)
+    #print(answer)
+
+    print(n)
+    print(parents)
 
 
 
-sys.setrecursionlimit(10**7)
-threading.stack_size(2**27)
+#sys.setrecursionlimit(10**7)
+#threading.stack_size(2**27)
 threading.Thread(target=main).start()
 
-main()
